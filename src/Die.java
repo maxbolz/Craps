865ace7b01e58b28765c2295897674daa5a52aae
maxbolz <maxbolz@billericak12.com>
2020-11-17 14:25:04 -0500

    Initial commit

---
+++ src/Die.java
@@ -0,0 +1,24 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Die {
+
+    private int numDots;
+
+    public void roll()
+    {
+        numDots = (int)(Math.random()*6+1);
+    }
+
+    public int getNumDots()
+    {
+        return numDots;
+    }
+
+    public static void main(String[] args)
+    {
+        Die die = new Die();
+        die.roll();
+        System.out.println(die.getNumDots());
+    }
+
+}
